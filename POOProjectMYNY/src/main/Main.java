81648a25ddcb4f463f5a99061420839c6fa5679e
JenjenParibou <slimaniyahia1@gmail.com>
2025-12-13 11:50:15 +0100

    added more stuff and comments

--- POOProjectMYNY/src/main/Main.java
+++ POOProjectMYNY/src/main/Main.java
@@ -4,19 +4,11 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int op;
-		
-		title();
-		print("1. Main menu. \n2. Options. \n3. Quit.");
-		print("Please choose an option by typing the number.");
-		
-		do { op = sc.nextInt();
-		} while (op < 1 || op > 3);
-		
+		titleScreen(); //procedure that contains what happens on the title screen
+		game();
 	}
 	
-	static public void title() {
+	static public void logo() {
 		System.out.println(" __  ____     ___   ___     __  _____           _           _     ");
 		System.out.println("|  \\/  \\ \\   / / \\ | \\ \\   / / |  __ \\         (_)         | |    "); 
 		System.out.println("| \\  / |\\ \\_/ /|  \\| |\\ \\_/ /  | |__) | __ ___  _  ___  ___| |_   ");
@@ -29,4 +21,33 @@ public class Main {
 	static public void print(String s) {
 		System.out.println(s);
 	}
+	
+	static public void titleScreen() {
+		int op;
+		Scanner sc = new Scanner(System.in);
+		
+		logo(); // procedure that draws the game's logo (we can change the name later, this is just a placeholder
+		print("1. New Game. \n2. Load. \n3. Quit.");
+		print("Please choose an option by typing a number."); // Loading does nothing for now, we'll fix it later
+		
+		do { op = sc.nextInt(); // User is prompted to type a number
+		} while (op < 1 || op > 3); //User may only type a number between 1 and 3
+		
+		switch (op) {
+		case 1:
+			game();
+			break;
+		case 2:
+			game();
+			break;
+		case 3:
+			print("See you later!");
+			break;
+		}
+	}
+	
+	static public void game() { // aka this is where everything happens
+		
+	}
+	
 }
