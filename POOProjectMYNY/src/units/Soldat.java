d1c2d55e1347d0559ba0da3fca0c0bcb5f4320fd
neylou <neyloubrim@gmail.com>
2025-12-20 21:09:12 +0100

    Add Unit and Soldat classes

---
+++ POOProjectMYNY/src/units/Soldat.java
@@ -0,0 +1,14 @@
+package units;
+
+public class Soldat extends Unit {
+    public Soldat() {
+        this.hp = 100;
+        this.atk = 30;
+        this.def = 20;
+        this.spd = 5;
+        this.range = 1;
+        this.cost = 50;
+        this.type = "Soldat";
+    }
+
+}
