60bc8f91c0b712d23b999c31e9a423845e410e23
neylou <neyloubrim@gmail.com>
2025-12-22 20:52:39 +0100

    added an archer unit and changed speed stat.

--- POOProjectMYNY/src/units/Soldat.java
+++ POOProjectMYNY/src/units/Soldat.java
@@ -5,9 +5,9 @@ public class Soldat extends Unit {
         this.hp = 100;
         this.atk = 30;
         this.def = 20;
-        this.spd = 5;
-        this.range = 1;
-        this.cost = 50;
+        this.spd = 1; //speed is how many tiles of the map a unit can move per turn
+        this.range = 1; //range is how many tiles the enemy has to be away to be attacked
+        this.cost = 50; //this'll be changed according to yudas's work on economy
         this.type = "Soldat";
     }
 
